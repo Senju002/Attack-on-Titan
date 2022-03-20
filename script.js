8300a7e09ccbd13713d9ed955e73a4b5c5129f24
Senju002 <99812437+Senju002@users.noreply.github.com>
2022-03-20 20:01:54 +0700

    add files

---
+++ script.js
@@ -0,0 +1,51 @@
+$(document).ready(function () {
+  $("#autoWidth").lightSlider({
+    autoWidth: true,
+    loop: true,
+    onSliderLoad: function () {
+      $("#autoWidth").removeClass("cS-hidden");
+    },
+  });
+});
+
+function reveal() {
+  var reveals = document.querySelectorAll(".reveal");
+  for (var i = 0; i < reveals.length; i++) {
+    var windowHeight = window.innerHeight;
+    var elementTop = reveals[i].getBoundingClientRect().top;
+    var elementVisible = 150;
+    if (elementTop < windowHeight - elementVisible) {
+      reveals[i].classList.add("active");
+    } else {
+      reveals[i].classList.remove("active");
+    }
+  }
+}
+window.addEventListener("scroll", reveal);
+
+mybutton = document.getElementById("myBtn");
+
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function () {
+  scrollFunction();
+};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 300 || document.documentElement.scrollTop > 300) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
+}
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+  window.scroll({ top: 0, behavior: "smooth" }); // For Chrome, Firefox, IE and Opera
+}
+
+let hamburger = document.getElementsByClassName("hamburger")[0];
+let navbarLinks = document.getElementsByClassName("navbar-links")[0];
+
+hamburger.addEventListener("click", () => {
+  navbarLinks.classList.toggle("active");
+});
